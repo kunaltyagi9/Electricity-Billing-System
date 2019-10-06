2b68c897e4cc4c9e794fd03d15a655756498a04c
Kunal Tyagi <kunaltyagi93@gmail.com>
2019-10-06 16:38:13 +0530

    added new java project

---
+++ Electricity-Billing-System/src/Electricity/conn.java
@@ -0,0 +1,19 @@
+package Electricity;
+
+import java.sql.*;  
+
+public class conn{
+    Connection c;
+    Statement s;
+    public conn(){  
+        try{  
+            Class.forName("com.mysql.jdbc.Driver");  
+            c =DriverManager.getConnection("jdbc:mysql:///project6","root","");    
+            s =c.createStatement();  
+            
+           
+        }catch(Exception e){ 
+            System.out.println(e);
+        }  
+    }  
+}  
